fa7682dfed76c104c21389b8e224e6491987062b
BezerraPedrinho <phdesign9@gmail.com>
2023-08-24 17:47:31 -0300

    1st test

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,8 @@
+///<reference types="Cypress" />
+describe('Central de Atendimento ao Cliente TAT', ()=>{
+  
+  it("verifaca o titulo da aplicação", ()=>{
+    cy.visit('./src/index.html')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+})
